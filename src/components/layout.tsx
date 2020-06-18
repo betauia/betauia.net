02bf7c7a550df908235028461bd3d112dd7e344e
Bendik Egenes Dyrli <bendik.dyrli@uia.no>
2020-06-18 12:46:58 +0200

    added some cometic changes along with some text changes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,37 +6,37 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import {Link} from 'gatsby';
+import { Link } from 'gatsby';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faSnapchat, faFacebook, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
-export default function Layout({children}){
-	return(<>
-		<Navbar variant="dark" style={{backgroundColor: "#0085ff"}}>
+export default function Layout({ children }) {
+	return (<>
+		<Navbar variant="dark" style={{ backgroundColor: "#0085ff" }}>
 			<Navbar.Brand>
 				<Link to="/">
-					<Image src="/logo_test.jpg" width="128" height="38" style={{verticalAlign: "top", marginRight: "0.2em"}}  />
+					<Image className="beta-logo" src="/logo_test.jpg" height="38" />
 				</Link>
 			</Navbar.Brand>
 			<Nav>
 				<Link to="/">Home</Link>
-				<Link to="/about">About</Link>
+				<a href="https://wiki.betauia.net/beta">About</a>
 			</Nav>
 		</Navbar>
 		{children}
-		<Container fluid style={{padding: "2em 0", backgroundColor: "#eeeeee"}}>
-			<Container style={{padding: "2em"}}>
-			<Row>
-				<Col style={{textDecoration: "underline"}}>Contact us on:</Col>
-			</Row>
-			<br />
-			<Row>
-				<Col><a href="#"><FontAwesomeIcon icon={faInstagram} />Instagram</a></Col>
-				<Col><a href="#"><FontAwesomeIcon icon={faSnapchat} />Snapchat</a></Col>
-				<Col><a href="#"><FontAwesomeIcon icon={faFacebook} />Facebook</a></Col>
-				<Col><a href="#"><FontAwesomeIcon icon={faDiscord} />Discord</a></Col>
-			</Row>
+		<Container className="footer" fluid >
+			<Container className="footer-breathing">
+				<Row>
+					<Col className="contact_aliens">Contact us on:</Col>
+				</Row>
+				<br />
+				<Row>
+					<Col><a href="https://www.instagram.com/betauia/"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></Col>
+					<Col><a href="https://www.snapchat.com/add/betauia"><FontAwesomeIcon icon={faSnapchat} /> Snapchat</a></Col>
+					<Col><a href="https://www.facebook.com/beta.linjeforening"><FontAwesomeIcon icon={faFacebook} /> Facebook</a></Col>
+					<Col><a href="https://discord.com/invite/UQCPkZD"><FontAwesomeIcon icon={faDiscord} /> Discord</a></Col>
+				</Row>
 			</Container>
 		</Container>
 	</>)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,57 +11,66 @@ import Layout from '../components/layout';
 // TODO: Scale down beta logo image
 
 export default function Home() {
-  return <div>
+	return <div>
 		<Layout>
 			<Jumbotron>
 				<Container>
-					<h1>BETA - Beta Engineering and Technology Association</h1>
-					<p>
-						This is the BETA website, it is a work in progress.
-					</p>
+					<h1><strong>B</strong>ETA <strong>E</strong>ngineering and <strong>T</strong>echnology <strong>A</strong>ssociation</h1>
+					<br></br>
+					<h5>
+						<ul className="listings">
+							<li>This is the BETA website, as you can see it's quite work in progress.</li>
+							<li>BETA is the student organization, for the Computer Engineering and Cybersecurity Students at <a href="https://uia.no">UiA Grimstad</a>.</li>
+							<ul className="listings">
+								<li> Alpha </li>
+								<li> Beta </li>
+								<li> Gamma? </li>
+							</ul>
+						</ul>
+					</h5>
 				</Container>
 			</Jumbotron>
 			<Container>
 				<Row>
 					<Col>
-					<Card>
-						<Card.Body>
-							<Card.Title>EventKom</Card.Title>
-							<Card.Text>Here is info</Card.Text>
-							<Card.Link><Button variant="link">Read more</Button></Card.Link>
-						</Card.Body>
-					</Card>
+						<Card>
+							<Card.Body>
+								<Card.Title>EventKom</Card.Title>
+								<Card.Text>Here is info</Card.Text>
+								<Card.Link><Button variant="link">Read more</Button></Card.Link>
+							</Card.Body>
+						</Card>
 					</Col>
 					<Col>
-					<Card>
-						<Card.Body>
-							<Card.Title>Bedkom</Card.Title>
-							<Card.Text>Here is info. Here is some more info. This is mostly to check out if the thingies do the thingy whith line endings.</Card.Text>
-							<Card.Link><Button variant="link">Read more</Button></Card.Link>
-						</Card.Body>
-					</Card>
+						<Card>
+							<Card.Body>
+								<Card.Title>BedKom</Card.Title>
+								<Card.Text>Here is info. Here is some more info. This is mostly to check out if the thingies do the thingy whith line endings.</Card.Text>
+								<Card.Link><Button variant="link">Read more</Button></Card.Link>
+							</Card.Body>
+						</Card>
 					</Col>
 					<Col>
-					<Card>
-						<Card.Body>
-							<Card.Title>Betadev</Card.Title>
-							<Card.Text>Here is info</Card.Text>
-							<Card.Link><Button variant="link">Read more</Button></Card.Link>
-						</Card.Body>
-					</Card>
+						<Card>
+							<Card.Body>
+								<Card.Title>BetaDev</Card.Title>
+								<Card.Text>Here is info</Card.Text>
+								<Card.Link><Button variant="link">Read more</Button></Card.Link>
+							</Card.Body>
+						</Card>
 					</Col>
 					<Col>
-					<Card>
-						<Card.Body>
-							<Card.Title>Security</Card.Title>
-							<Card.Text>Here is info</Card.Text>
-							<Card.Link><Button variant="link">Read more</Button></Card.Link>
-						</Card.Body>
-					</Card>
+						<Card>
+							<Card.Body>
+								<Card.Title>BetaSec</Card.Title>
+								<Card.Text>Here is info</Card.Text>
+								<Card.Link><Button variant="link">Read more</Button></Card.Link>
+							</Card.Body>
+						</Card>
 					</Col>
 				</Row>
 			</Container>
 			<br />
 		</Layout>
-    </div>
+	</div>
 }
