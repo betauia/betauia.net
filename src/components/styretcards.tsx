ceacab079630d738a5554f1f5af3b02942b7f85a
Ydalir <ferdil17@student.uia.no>
2020-06-20 21:47:05 +0200

    added leadership info

---
+++ src/components/styretcards.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+export interface Info {
+	Img: any,
+	Name: string,
+	Position: string,
+	Email: string,
+	Body?: string
+}
+
+interface StyretCardsProps {
+	info: Info[]
+}
+
+export default function StyretCards(props: StyretCardsProps) {
+	const mapped: JSX.Element[] = props.info.map((value: Info) => {
+		return <Col>
+				<Image src={value.Img} height="150" roundedCircle />
+				<h3>{value.Name}</h3>
+				<h5>{value.Position}</h5>
+				<strong>Email: <a href={"mailto:"+value.Email}>{value.Email}</a></strong>
+				<p>
+					{value.Body}
+				</p>
+		</Col>
+	});
+
+	const perChunk: number = 4;
+	const split: JSX.Element[][] = mapped.reduce((acc, value, index: number) => {
+		const chunkIndex: number = Math.floor(index/perChunk);
+		if(!acc[chunkIndex]){
+			acc[chunkIndex] = [];
+		}
+
+		acc[chunkIndex].push(value);
+		return acc;
+	}, [])
+
+	return <>{split.map((value) => {
+		return <Row>
+			{value}
+		</Row>
+	})}</>;
+}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,46 +3,59 @@ import Layout from '../components/layout';
 import Container from 'react-bootstrap/Container';
 
 import Logo from '../../assets/beta-logo.png';
-import StyretCard, {StyretInfo} from '../components/styretcard';
+import StyretCards, {Info} from '../components/styretcards';
 
-const info: StyretInfo[] = [
+const StyretInfo: Info[] = [
 	{
 		Img: Logo,
-		Name: "Even Eilertsen",
-		Body: "Here is info about Even Eilertsen"
+		Name: "Anders Godstad",
+		Position: "Leder",
+		Email: "leder@betauia.net"
+	},
+	{
+		Img: Logo,
+		Name: "Theodor Middleton",
+		Position: "Nestleder",
+		Email: "nestleder@betauia.net"
+	},
+	{
+		Img: Logo,
+		Name: "Vinh Hu",
+		Position: "Økonomi ansvarlig",
+		Email: "okonomi@betauia.net"
+	},
+	{
+		Img: Logo,
+		Name: "Ingrid Louise Husevåg-Kristensen",
+		Position: "Bedriftskomité leder",
+		Email: "bedrift@betauia.net"
+	},
+	{
+		Img: Logo,
+		Name: "Jon Inntveit",
+		Position: "Eventkomité leder",
+		Email: "event@betauia.net"
+	},
+	{
+		Img: Logo,
+		Name: "Adrian Risholm",
+		Position: "BetaLAN leder",
+		Email: "betalan@betauia.net",
 	},
 	{
 		Img: Logo,
 		Name: "Even Eilertsen",
-		Body: "Here is info about Even Eilertsen"
-	},
-	{
-		Img: Logo,
-		Name: "Even Eilertsen",
-		Body: "Here is info about Even Eilertsen"
-	},
-	{
-		Img: Logo,
-		Name: "Even Eilertsen",
-		Body: "Here is info about Even Eilertsen"
-	},
-	{
-		Img: Logo,
-		Name: "Even Eilertsen",
-		Body: "Here is info about Even Eilertsen"
-	},
-	{
-		Img: Logo,
-		Name: "Even Eilertsen",
-		Body: "Here is info about Even Eilertsen"
-	},
+		Position: "BetaSec leder",
+		Email: "sikkerhet@betauia.net"
+	}
 ]
 
 export default function About() {
 	return <>
 	<Layout>
 		<Container className="styret">
-			<StyretCard info={info} />
+			<h1>Styret</h1>
+			<StyretCards info={StyretInfo} />
 		</Container>
 	</Layout>
 	</>
