5e56df8975bfe5b911f87e0e9d45841d36e42406
Jakob Moen Bekken <jakobmb@uia.no>
2023-11-08 16:44:41 +0100

    Changes and fixes to paths that mixes

---
+++ app/static/js/admin.js
@@ -0,0 +1,140 @@
+function updateOutput() {
+    // Updates markdown output
+    const inputText = document.getElementById('inputBox').value;
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = marked(inputText);
+}
+
+function clearSection() {
+    // Clears Section selection and input 
+    document.getElementById('inputBox').value = '';
+    document.getElementById('sectionTitle').innerText = 'Select Section';
+    currentSection = null;
+    updateOutput();
+}
+
+function handleLogin() {
+    // Checking if the user entered anything as username and password (not secure)
+    // TODO: check need server-side validation
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username && password) {
+        document.getElementById("loginForm").style.display = "none";
+        document.getElementById("editor").style.display = "block";
+    } else {
+        alert("Please enter a username and password.");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    // Reference to the dropdown items and the markdown input textarea
+    const dropdownItems = document.querySelectorAll(".dropdown-content a");
+    const inputBox = document.getElementById("inputBox");
+
+    // Maps dropdown to markdown files
+    // TODO: needs to be replaced by server side mapping
+    const markdownFileMapping = {
+        "Beta": "/static/md/beta-2023h.md",
+        "BetaDev": "/static/md/betadev-2023h.md",
+        // TODO: add more mappings for other sections
+    };
+
+    let currentSection = null; // Selected section
+
+    async function saveMarkdownContent() {
+        const markdownContent = document.getElementById('inputBox').value;
+
+        if (!currentSection) {
+            alert('Select a section before saving!');
+            return;
+        }
+
+        const saveUrl = `post/save`; // TODO: spør backend om riktig path
+        const currentDateAndTime = new Date();
+
+        // Adjust for CEST time
+        const offset = currentDateAndTime.getTimezoneOffset() / 60;
+        const CESTOffset = 2; // CEST is UTC+2
+        currentDateAndTime.setHours(currentDateAndTime.getHours() + offset + CESTOffset);
+
+        const currentDate = currentDateAndTime.toISOString().split('T')[0];
+        const currentTime = currentDateAndTime.toISOString().split('T')[1].split('.')[0]; // Removes milliseconds
+
+        console.log('Date: ', currentDate, '\nTime: ', currentTime);
+
+
+        try {
+            const rawResponse = await fetch(saveUrl, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+
+                    body: markdownContent,
+                    section: currentSection,
+                    date: currentDate,
+                    time: currentTime
+                })
+            });
+
+            const data = await rawResponse.json();
+
+            if (data.success) {
+                alert('Changes saved successfully');
+            } else {
+                alert('Error occurred');
+            }
+        } catch (error) {
+            console.log('Error saving changes.', error);
+            alert('Error.');
+        }
+    }
+
+
+    // Binding the function to a button click event:
+    document.getElementById('saveButton').addEventListener('click', saveMarkdownContent);
+
+    // click listeners to each dropdown item
+    dropdownItems.forEach(item => {
+        item.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            const sectionName = event.target.innerText;
+
+            // Fetch the Markdown content for the selected section
+            fetchMarkdownContent(sectionName);
+
+            // Update the current section
+            currentSection = event.target.getAttribute("data-section");
+
+            // Update the dropdown button text
+            document.querySelector(".dropdown-trigger .button span").textContent = event.target.innerText;
+        });
+    });
+
+
+
+
+    function fetchMarkdownContent(sectionName) {
+        const filePath = markdownFileMapping[sectionName];
+        if (!filePath) {
+            console.error(`No markdown file path found for section: ${sectionName}`);
+            return;
+        }
+
+        // Fetch the markdown content from the file
+        fetch(filePath)
+            .then((response) => response.text())
+            .then((markdownContent) => {
+                inputBox.value = markdownContent;
+                updateOutput(); // Update md output
+            })
+            .catch((error) => {
+                console.error("Error fetching Markdown content:", error);
+            });
+    }
+});
---
+++ app/static/md/betadev-2023h.md
@@ -0,0 +1,60 @@
+# Hva er BetaDev?
+BetaDev er en interessegruppe under linjeforeningen Beta. Vi fokuserer på utvikling av prosjekter, spill og å lære av hverandre. Dette oppnår vi blant annet ved å arrangere game jam! Side for tidligere spill [her](https://betauia.net/betadev/betadev-2023h.html)
+
+
+---
+# BetaDev høsten 2023
+Her finner du informasjon om interessegruppen BetaDev for høsten 2023. Vi arrangerer ting som har med utvikling og programmering å gjøre.
+
+Ting å følge med på høsten 2023:
+- **Game jam 1**: 7. september, start
+- **Game jam 2**: 20. oktober, start
+- Møte for interesserte: hver **onsdag kl 16:15**
+    - Kode-prosjekt
+
+## Hva er game jam
+Game Jam er en sprint i å lage spill. Du har en uke på deg, på starten av uka får alle *tema*, også jobber du helt til kåringen på slutten. Tema er likt for alle, og kan være et enkelt ord. Eksempler fra [Global Game Jam](https://globalgamejam.org/history): *deception, repair, duality*.
+
+Game Jam er en øvelse i å jobbe under press, samarbeide og å være kreativ.
+
+# Game jam 1
+Hvis du vil være med på Game Jam 1 kan du melde deg på [her](https://forms.office.com/e/Eze1w2mi2P), enten som gruppe eller alene. Du trenger kun å møte opp fysisk på slutten, da vi skal kåre spillet ditt.
+
+Tema dropper [her](./betadev/tema.html) kl 12:00 7. september, og vi kommer også til å ha fysisk møte kl 18:15 i rom A2-021. Her kan du få informasjon, hjelp til å starte, og finne andre hvis du trenger noen å jobbe med.
+
+Vi har fysisk kåring **fredag 15. september** på rom A2-021. Deltakerne får prøve andres spill, og vise fram sitt eget. På slutten har vi avstemning, og en premie på **1000 kr**!
+
+## Krav
+Du kan lage spillet nesten hvordan du vil. Stjel deler fra andre, bruk ChatGPT, lag et brettspill hvis du vil det! Men husk at andre skal kunne spille det på kåring, og husk å knytte det opp mot *tema* på en eller annen måte. 
+
+Her er redskaper verdt å sjekke ut for de som lager dataspill:
+- [pygame](https://www.pygame.org/)
+    - python library, gratis
+    - hjelper med 2d graphics, user input, lyd
+    - kjapt å bruke
+- [pixiJS](https://pixijs.com/) og [p5.js](https://p5js.org/)
+    - javascript library, gratis
+    - hjelper med 2d graphics, user input, lyd
+- [bevy](https://bevyengine.org/)
+    - rust library, gratis
+    - hjelper med 2d og 3d graphics, user input, lyd, UI
+- [unity](https://unity.com/)
+    - full game engine, gratis for privatpersoner
+    - bruker C# for scripting
+- [godot](https://godotengine.org/)
+    - full game engine, gratis
+    - bruker hovedsaklig C++ for scripting
+
+# game jam 2
+...
+
+## kode-prosjekt
+Vi jobber med å forbedre denne nettsiden. Hvis du vil være med, kan du komme på møte hver onsdag kl 16:15.
+
+# kontakt oss
+Ønsker du å være med på noen av prosjektene våre, eller lurer på noe, finner du oss her:
+- discord: [beta kanal](https://discord.gg/v8b6ZrQ")
+- mail: `betadev@betauia.net`
+- leder: Ask Sødal
+    - mail: `asks@uia.no` 
+    - discord: `kjell_tore`
