4ee794b86013a11bdeeef2fd358459e576ec2c6b
Jakob Moen Bekken <jakobmb@uia.no>
2024-02-14 00:22:21 +0100

    Added .env for Flask
    Made Flask use the same .env file as Postgres.
    TODO: Make it a developer .env.dev file and a hide the .env in production.

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,8 +1,9 @@
 import psycopg2 as pg
+import os
 
 conn = pg.connect(
-    host="postgres",
-    database="testdb",
-    user="user",
-    password="1234"
+    host=os.environ.get("POSTGRES_HOST"),
+    database=os.environ.get("POSTGRES_DB"),
+    user=os.environ.get("POSTGRES_USER"),
+    password=os.environ.get("POSTGRES_PASSWORD")
 )
