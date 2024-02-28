7daec0a2a9229323c07123e60dabf4c859ec189f
Aryan Aziz Mathiesen <aryanam@uia.no>
2024-02-28 16:38:42 +0100

    Updated get API

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,18 +1,26 @@
 const modal1 = document.getElementById("myModal1");
 const btn1 = document.getElementById("myBtn1");
-const closeBtn1 = document.getElementById("myModal1").getElementsByClassName("close")[0];
+const closeBtn1 = document
+  .getElementById("myModal1")
+  .getElementsByClassName("close")[0];
 
 const modal2 = document.getElementById("myModal2");
 const btn2 = document.getElementById("myBtn2");
-const closeBtn2 = document.getElementById("myModal2").getElementsByClassName("close")[0];
+const closeBtn2 = document
+  .getElementById("myModal2")
+  .getElementsByClassName("close")[0];
 
 const modal3 = document.getElementById("myModal3");
 const btn3 = document.getElementById("myBtn3");
-const closeBtn3 = document.getElementById("myModal3").getElementsByClassName("close")[0];
+const closeBtn3 = document
+  .getElementById("myModal3")
+  .getElementsByClassName("close")[0];
 
-const openHamDOM = document.querySelector('#openHam');
-const closeHamDOM = document.querySelector('#closeHam');
-const navigationItemsDOM = document.querySelector('#navigation-items');
+const markdownContainerDOM = document.querySelector(".markdown-container");
+
+const openHamDOM = document.querySelector("#openHam");
+const closeHamDOM = document.querySelector("#closeHam");
+const navigationItemsDOM = document.querySelector("#navigation-items");
 
 // Open Modal 1
 btn1.onclick = function () {
@@ -46,7 +54,11 @@ closeBtn3.onclick = function () {
 
 // Close modals when clicked outside of the modals
 window.onclick = function (event) {
-  if (event.target == modal1 || event.target == modal2 || event.target == modal3) {
+  if (
+    event.target == modal1 ||
+    event.target == modal2 ||
+    event.target == modal3
+  ) {
     modal1.style.display = "none";
     modal2.style.display = "none";
     modal3.style.display = "none";
@@ -54,110 +66,131 @@ window.onclick = function (event) {
 };
 
 // Tab function
-function openTab(evt, tabName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("content-tab");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tab");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].classList.remove("is-active"); // Remove "is-active" from all tabs
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.classList.add("is-active"); // Add "is-active" to the clicked tab
+async function openTab(evt, tabName) {
 
-  // Store the active tab in localStorage
+  const contentTabs = document.querySelectorAll(".content-tab");
+  contentTabs.forEach(tab => {
+    tab.style.display = "none";
+  });
+
+  // Deactivate all tabs
+  const tablinks = document.querySelectorAll(".tab");
+  tablinks.forEach(tab => {
+    tab.classList.remove("is-active");
+  });
+
+  const selectedContentTab = document.getElementById(tabName);
+  if (selectedContentTab) {
+    selectedContentTab.style.display = "block";
+    evt.currentTarget.classList.add("is-active");
+  }
+
+  // Store active tab in localstorage
   localStorage.setItem("activeTab", tabName);
+
+  fetchAndDisplayMarkdown(tabName);
 }
 
 function setActiveTabOnLoad() {
-  var activeTab = localStorage.getItem("activeTab");
-  var contentTabs = document.getElementsByClassName("content-tab");
 
-  // Hide all content tabs initially
-  for (var i = 0; i < contentTabs.length; i++) {
-    contentTabs[i].style.display = "none";
+  const activeTab = localStorage.getItem("activeTab") || "tab";
+  const tabButton = document.querySelector(`.tab[onclick*="{${activeTab}"]`);
+
+  if (tabButton) {
+    // Simulate a tab click event
+    openTab({ currentTarget: tabButton }, activeTab);
+  } else {
+    const defaultTabButton = document.querySelector('.tab');
+    if (defaultTabButton) {
+      // Extracts tabname from onclick attribute
+      const defaultTabName = defaultTabButton.getAttribute('onclick').match(/'([^']+)'/)[1];
+      openTab({ currentTarget: defaultTabButton }, defaultTabName);
+    }
   }
+}
 
-  if (activeTab) {
-    // Remove "is-active" from all tabs
-    var tablinks = document.getElementsByClassName("tab");
-    for (var i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove("is-active");
+const fetchAndDisplayMarkdown = async (tabName) => {
+  // TODO: Add a loading cycle while this runs
+  try {
+    // Capitalizes tabname and sets other characters to lowercase
+    const cap_tabName = tabName[0] + tabName.slice(1).toLowerCase();
+    const converter = new showdown.Converter();
+    const {
+      data: { posts },
+    } = await axios.get(`/post/all?branch=${cap_tabName}`);
+
+    if (posts.length < 1) {
+      markdownContainerDOM.innerHTML = '<h5 class="empty-list">No posts yet</h5>';
+      return;
     }
 
-    // Set the active tab from localStorage as "is-active"
-    document.getElementById(activeTab).style.display = "block";
-    document.querySelector(`[onclick*="${activeTab}"]`).classList.add("is-active");
+    const sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+    const latestPostID = sortedPosts[0].id;
 
-    // Render the markdown content for the active tab
-    fetchAndDisplayMarkdown(activeTab);
-  } else {
-    // If no tab is set in localStorage, set a default tab as active and render its content
-    openTab("Beta");
-    fetchAndDisplayMarkdown("Beta");
+    const {
+      data: { post },
+    } = await axios.get(`/api/post/${latestPostID}`);
+
+    const { title, body, time, date } = post;
+
+
+
+    // Get content by branch which is also tabname
+    const totalHTML = `
+
+      <div id="${tabName}" class="content-tab" style="display: none;">
+              <div id="markdown-content" class="subtitle is-5 is-beta-white subtitle-text italic custom-markdown"
+                style="margin: auto; width: 100%; ">
+                <!-- Title -->
+                <h2>${title}</h2>
+                <!-- Body --> 
+                ${converter.makeHTML(body)}
+
+                <!-- Time -->
+                <h5>Created: ${date} ${time}</h5>
+          </div>
+      </div>
+      `;
+
+    markdownContainerDOM.innerHTML = tabName;
+
+  } catch (error) {
+    console.error(error);
+    document.getElementById(tabName).innerHTML = "<h5>There was an error:(</h5>";
   }
-}
+};
 
-
-async function fetchAndDisplayMarkdown(tabName) {
-  const converter = new showdown.Converter();
-  const markdownFileMapping = {
-    "Beta": "Beta regular boring snooze zzzz",
-    "BetaDev": "BetaDev stuff yes happy",
-    "BetaSec": "BetaSec mr hacky bois",
-    "BetaLan": "BetaLan we play game",
-    "BedKom": "BetaKom we need more pizza plz"
-  };
-
-  const markdownFilename = markdownFileMapping[tabName];
-  if (!markdownFilename) {
-    console.error(`No markdown filename found for tab: ${tabName}`);
-    return;
-  }
-
-  // Update the fetch URL to point to your Flask server's route
-  // TODO: remove this shit boi
-  const htmlContent = converter.makeHtml(markdownFilename);
-  document.getElementById(tabName).querySelector(".custom-markdown").innerHTML = htmlContent;
-}
+setActiveTabOnLoad();
 
 /* Light/Dark mode */
 
-document.addEventListener('DOMContentLoaded', () => {
 
-  setActiveTabOnLoad();
 
-  const tabButtons = document.querySelectorAll("[data-tab]");
-  tabButtons.forEach(button => {
-    button.addEventListener("click", function (e) {
-      const tabName = e.target.getAttribute("data-tab");
-      openTab(e, tabName);
-      fetchAndDisplayMarkdown(tabName);
-    });
-  });
-});
-
-const modeToggle = document.getElementById('modeToggle');
-const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
-let theme = localStorage.getItem('theme');
+const modeToggle = document.getElementById("modeToggle");
+const userPrefersLight =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: light)").matches;
+let theme = localStorage.getItem("theme");
 
 // Set initial theme based on user preference or saved theme
-if (theme === 'light' || (theme === null && userPrefersLight)) {
-  document.body.classList.add('light-mode');
-  modeToggle.innerHTML = '<span class="icon"><i class="fas fa-moon"></i></span>';
+if (theme === "light" || (theme === null && userPrefersLight)) {
+  document.body.classList.add("light-mode");
+  modeToggle.innerHTML =
+    '<span class="icon"><i class="fas fa-moon"></i></span>';
 } else {
   modeToggle.innerHTML = '<span class="icon"><i class="fas fa-sun"></i></span>';
 }
 
-modeToggle.addEventListener('click', function () {
-  document.body.classList.toggle('light-mode');
-  let newTheme = document.body.classList.contains('light-mode') ? 'light' : 'dark';
-  localStorage.setItem('theme', newTheme);
-  this.innerHTML = newTheme === 'light' ?
-    '<span class="icon"><i class="fas fa-moon"></i></span>' :
-    '<span class="icon"><i class="fas fa-sun"></i></span>';
+modeToggle.addEventListener("click", function () {
+  document.body.classList.toggle("light-mode");
+  let newTheme = document.body.classList.contains("light-mode")
+    ? "light"
+    : "dark";
+  localStorage.setItem("theme", newTheme);
+  this.innerHTML =
+    newTheme === "light"
+      ? '<span class="icon"><i class="fas fa-moon"></i></span>'
+      : '<span class="icon"><i class="fas fa-sun"></i></span>';
 });
 
 let isMenuOpen = false;
@@ -166,24 +199,26 @@ const hamburgerEvent = (nav, close, open) => {
   navigationItemsDOM.style.display = nav;
   closeHamDOM.style.display = close;
   openHamDOM.style.display = open;
-  isMenuOpen = nav === 'flex';
-}
+  isMenuOpen = nav === "flex";
+};
 
-openHamDOM.addEventListener('click', () => hamburgerEvent('flex', 'block', 'none'));
-closeHamDOM.addEventListener('click', () => hamburgerEvent('none', 'none', 'block'));
+openHamDOM.addEventListener("click", () =>
+  hamburgerEvent("flex", "block", "none")
+);
+closeHamDOM.addEventListener("click", () =>
+  hamburgerEvent("none", "none", "block")
+);
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   if (window.innerWidth > 768) {
     hamburgerEvent("flex", "none", "block");
-  }
-  else if (window.innerWidth < 768) {
+  } else if (window.innerWidth < 768) {
     hamburgerEvent("none", "none", "block");
   }
-})
+});
 
-
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   if (screen.width < 768) {
-    hamburgerEvent('none', 'none', 'block');
+    hamburgerEvent("none", "none", "block");
   }
 });
