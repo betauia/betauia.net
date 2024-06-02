eaa8469311a4ea9cfb7c4594bea59736cc59a83a
Jakob Moen Bekken <jakobmb@uia.no>
2024-06-02 04:40:57 +0200

    Made production ready and configured NGINX
    Made server ready for deployment with gunicorn. Now possible to run either default dev server, or the production server with using both compose files.
    NGINX configured to keep up the ip, names, hosts, and other stuff relevant to make good standard requests.
    Static files is getting reloaded, but this isnt a problem, might just be NGINX not caching it. Easily configurable.

--- flask/server.py
+++ flask/server.py
@@ -1,5 +1,6 @@
 from app import create_app
 
-application = create_app()
+app = create_app()
+
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=9091, debug=True)
+    app.run(host="0.0.0.0", port=9091, debug=True)
