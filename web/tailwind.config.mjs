2df3f0829b192d6db9b21f6ed01a6f62760f214b
Jakob Moen Bekken <jakobmb@uia.no>
2024-07-24 00:12:52 +0200

    Made a layout for pages
    Made a footer and navbar to wrap the pages in. Stil not linked all footer elements and needs some content. Also made an example of a sponsor spot in the footer.

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        "brand": "#0085ff"
+      }
+    },
 	},
 	plugins: [],
 }
