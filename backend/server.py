ad99152b7dadb7bbaea59776ed735f266b69cc3f
Jakob Moen Bekken <jakobmb@uia.no>
2025-05-31 12:39:29 +0200

    ♻️ Move Flask prod startup to Dockerfile

--- backend/server.py
+++ backend/server.py
@@ -5,13 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
 
-    if os.getenv("FLASK_ENV", "development") == "production":
-        import subprocess
-        subprocess.run([
-            "gunicorn",
-            "-w", "4",
-            "-b", "0.0.0.0:8000",
-            "server:app"
-        ])
-    else:
-        app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
