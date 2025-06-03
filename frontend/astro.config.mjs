a60435f64bb62f43edead8d6e9a9268a6842d47e
Jakob Moen Bekken <jakobmb@uia.no>
2025-06-04 00:07:08 +0200

    ✨ Added FA icons

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), icon()],
   server: { port: 3000, host: "0.0.0.0" }
 });
