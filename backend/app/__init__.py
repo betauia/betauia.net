af73f930749ae58572b832b4dc02ea28b910bed6
Filip Nilsen <filipn@uia.no>
2025-06-05 23:41:58 +0200

    🚚 Jokko wanted me to move everything into routes.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,6 +6,6 @@ def create_app():
     from app.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from app.calendar import calendarBlueprint
+    from app.routes import calendarBlueprint
     app.register_blueprint(calendarBlueprint)
     return app
--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,1 +1,2 @@
 from .main import main
+from .calendar import calendarBlueprint
