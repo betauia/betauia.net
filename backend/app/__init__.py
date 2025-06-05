2c7d11d3853da1dc89e3f374974cb0dd554da80f
Filip Nilsen <filipn@uia.no>
2025-06-05 22:50:45 +0200

    ✨ Add api that can get an icalendar file and convert it to a json. For  the moment it grabs the calendar from google.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 def create_app():
-  app = Flask(__name__)
+    app = Flask(__name__)
 
-  from app.routes import main as main_blueprint
-  app.register_blueprint(main_blueprint)
+    from app.routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-  return app
+    from app.calendar import calendarBlueprint
+    app.register_blueprint(calendarBlueprint)
+    return app
