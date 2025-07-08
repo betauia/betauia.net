dcb325d1a71193ea1673e17f066c4cf5f412b13d
Aris <99477476+PinOstrich@users.noreply.github.com>
2025-07-08 10:38:32 +0000

    ✨ Added LaTeX support
    Co-authored-by: Jakob Moen Bekken <jakobmb@uia.no>

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon()],
-  server: { port: 3000, host: "0.0.0.0" }
+  server: { port: 3000, host: "0.0.0.0" },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
