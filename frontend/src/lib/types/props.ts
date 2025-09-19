9141083da18a0e11b6561ab7b8e0df34c4349907
Jakob Moen Bekken <jakobmb@uia.no>
2025-09-19 10:25:21 +0000

    🩹 Moved banner into reusable component

--- frontend/src/lib/types/props.ts
+++ frontend/src/lib/types/props.ts
@@ -1,5 +1,10 @@
 import type { Pdf } from "@lib/types/models";
 
+export interface BannerProps {
+  text: string;
+  link: string;
+  buttonText?: string;
+}
 export interface SortListProps {
   files: Pdf[];
 }
