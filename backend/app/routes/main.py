fcfc9a3ef07d5439a587cdf0855dac77ff1d055e
Jakob Moen Bekken <jakobmb@uia.no>
2025-11-06 20:17:08 +0100

    ✨ Add db integration in backend

--- backend/app/routes/main.py
+++ backend/app/routes/main.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, jsonify
 
+from app.db import get_db
+
 main_bp = Blueprint("main", __name__)
 
 
 @main_bp.route("/ping")
 def ping():
     return jsonify({"message": "pong"})
+
+
+@main_bp.route("/db")
+def db_test():
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT 1;")
+    result = cur.fetchone()[0]
+    cur.close()
+    return {"db": result}, 200
