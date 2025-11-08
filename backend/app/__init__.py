d4a117770ed1dbf03335b3afae562e8798cd5dd6
Jakob Moen Bekken <jakobmb@uia.no>
2025-11-09 00:19:17 +0100

    ✨ Add rate limiting

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_limiter.errors import RateLimitExceeded
 
 from app.config import Config
 from app.db import close_db, init_db
+from app.limiter import limiter
 from app.routes.calendar import calendar_bp
 from app.routes.main import main_bp
 
 
 def create_app(config_object=Config):
     app = Flask(__name__)
-
     app.config.from_object(config_object)
+
+    limiter.init_app(app)
+
+    @app.errorhandler(RateLimitExceeded)
+    def ratelimit_handler(e):
+        return jsonify({"error": "Too many requests"}), 429
 
     init_db(app)
     app.teardown_appcontext(close_db)
