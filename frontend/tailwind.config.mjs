23aabe2a3869b038cec4ca6473524a872c8d338c
Jakob Moen Bekken <61628712+jakobbekken@users.noreply.github.com>
2026-01-11 17:54:26 +0100

    ✨ Add genfors page (#158)
    * ✨ Add prose components
    
    * ✨ Add genfors page

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import("tailwindcss").Config} */
+
+import typography from "@tailwindcss/typography";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
+  plugins: [typography],
   theme: {
     extend: {
       fontFamily: {
