acadcfb48fa7c9fa9db358e9f8295c8fbec68233
Jakob Moen Bekken <jakobmb@uia.no>
2026-03-04 14:47:34 +0100

    🧑‍💻 Add reload on save for backend

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,9 @@
 from app import create_app
+from app.config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=Config.DEBUG)
